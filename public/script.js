457ecd4ff0db9cdaa93eb4b413b46ad4553e2aff
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-15 15:30:27 +0000

    modified scriptjs

--- public/script.js
+++ public/script.js
@@ -1,5 +1,15 @@
 document.getElementById('searchButton').addEventListener('click', () => {
     const countryName = document.getElementById('countryInput').value 
     // console.log(countryName) 
+    fetchCountryData(countryName)
 
 }) 
+
+async function fetchCountryData(countryName) {
+    try {
+        const response = await fetch(`/search?country=${countryName}`)
+        const data = response.json()
+    } catch (error) {
+        
+    }
+}
