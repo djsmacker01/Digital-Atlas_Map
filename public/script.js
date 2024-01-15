848d98b4e4f33d25cba01d4835c5233a029832f5
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-15 17:10:37 +0000

    new commit

--- public/script.js
+++ public/script.js
@@ -1,15 +1,18 @@
 document.getElementById('searchButton').addEventListener('click', () => {
     const countryName = document.getElementById('countryInput').value 
-    // console.log(countryName) 
+    console.log(countryName) 
     fetchCountryData(countryName)
 
 }) 
 
 async function fetchCountryData(countryName) {
+    const messageElement = document.getElementById('message')
     try {
         const response = await fetch(`/search?country=${countryName}`)
         const data = response.json()
+        console.log(data)
     } catch (error) {
-        
+        console.log(error)
+        messageElement.textContent = "No result Found. Please check your spelling and try again."
     }
 }
