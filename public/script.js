dd84e668967e99dba07b013e48c63cb6ccb8e254
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-18 09:24:05 +0000

    Modified js

--- public/script.js
+++ public/script.js
@@ -69,7 +69,7 @@ function displayCountryInfo(countryData, container) {
 
     `
 
-    container.innerHTML = listHtml
+    // container.innerHTML = listHtml
     document.getElementById('flagContainer').innerHTML = flagUrl ? `<p><strong>Flag:</strong></p><img src="${flagUrl}" alt="${flagAlt}">` : ''
     document.getElementById('coatOfArmsContainer').innerHTML = coatOfArmsUrl? `<p><strong>Coat of Arms: </strong></p><img src="${coatOfArmsUrl}" alt="Coat of Arms of ${countryData.name.common}">` : ''
 }
